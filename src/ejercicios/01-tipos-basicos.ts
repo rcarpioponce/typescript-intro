42bb8d7ccf4a8edc46bb66bb962481fd07085eb4
Renzo Carpio Ponce <me@renzocarpio.com>
2021-01-23 22:07:09 -0500

    01 tipos basicos

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,13 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+
+nombre = 'Renzo';
+hp = 'FULL';
+
+console.log( nombre, hp, estaVivo );
